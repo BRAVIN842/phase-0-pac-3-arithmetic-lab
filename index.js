8199ee167bed0f0c4e8c4cbe5b23c47dd4d3b456
Bravin <onsasebravin853@gmail.com>
2023-09-26 18:46:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a + b   
+}
+function subtract(a,b) {
+    return a - b
+}
+function multiply(a,b) {
+    return a * b
+}
+function divide(a,b) {
+    return a / b
+}
+function increment(n) {
+    return n + 1
+}
+function decrement(n) {
+    return n - 1
+}
+function makeInt(n) {
+
+}
+function preserveDecimal(n) {
+
+}
+function makeInt(n) {
+    return parseInt(n, 10); // Parse n as an integer in base 10 (decimal).
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n); // Parse n as a floating-point number to preserve decimals.
+  }
+  
